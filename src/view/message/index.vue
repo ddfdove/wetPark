ab3ca3b11d78bb15c7adbcdba162ef06b0521ccb
转瞬之间 <10703354+ddf338@user.noreply.gitee.com>
2024-07-30 09:18:57 +0800

    .

--- src/view/message/index.vue
+++ src/view/message/index.vue
@@ -1,21 +1,10 @@
 <template>
-  <div class="flex" style="color: #ffffff;height: 100%;">
-    <div class="flex-1" style="height: 100%;">
-      <ul class="flex flex-col" style="height: 100%;">
-        <li class="flex-1">1</li>
-        <li class="flex-1">2</li>
-        <li class="flex-1">3</li>
-        <li class="flex-1">4</li>
-
-      </ul>
-    </div>
-    <Carousel class="flex-1"></Carousel>
-  </div>
-
+ 
 </template>
 
 <script setup>
-import Carousel from '../bird/carousel.vue'
+ 
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+</style>
